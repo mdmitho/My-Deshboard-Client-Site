7b5eedeec9c005082cdd9bb7b73d81e6483229f6
Hossen <mdmitho199911@gmail.com>
2022-05-31 23:37:56 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className='text-2xl font-bold text-purple-500'>Hello boss</h1>
+    </div>
+  );
+}
+
+export default App;
